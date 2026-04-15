b6297a1629b7532856f53c694dadea85aa675ddc
Abhishek H N <1NC25MC001@ncetmail.com>
2026-04-15 14:25:54 +0530

    Implement AI text summarizer with Transformers
    Added a text summarization feature using the Transformers library.

--- App.py
+++ App.py
@@ -1,1 +1,8 @@
 import streamlit as st
+from transformers import pipeline
+def load_summarizer():
+    returns pipeline("Summarization", model="Falconsai/text_summarization")
+  summarizer = load_summarizer()
+
+st.title(" AI text Summarizer")
+st.write("Enter a long text below, and get a concise summary!")
